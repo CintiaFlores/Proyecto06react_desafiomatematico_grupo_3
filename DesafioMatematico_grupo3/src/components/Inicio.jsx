0c6f222644b9ba0b5c881b6fbcbcba49352ad23c
Rodrigo Fines <112434197+Rodrigooo08@users.noreply.github.com>
2024-10-22 00:24:15 -0300

    Update Inicio.jsx

--- DesafioMatematico_grupo3/src/components/Inicio.jsx
+++ DesafioMatematico_grupo3/src/components/Inicio.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Inicio = ({ onStart }) => {
   return (
     <div className="inicio">
-      <h1>Desafío Matemático</h1>
+      
       <button onClick={onStart}>Empezar</button>
     </div>
   );
