37e06e1aaa048e519309482889202f078c41252c
Rodrigo Fines <112434197+Rodrigooo08@users.noreply.github.com>
2024-10-22 00:21:44 -0300

    Actualizacion en el css

--- DesafioMatematico_grupo3/src/components/Puntuacion.jsx
+++ DesafioMatematico_grupo3/src/components/Puntuacion.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Puntuacion = ({ puntos }) => {
   return (
-    <div>
+    <div className='final'>
       <h2>¡Juego terminado!</h2>
       <h3>Puntuación final: {puntos}</h3>
     </div>
